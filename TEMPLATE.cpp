7f1f4bfedefd0b95a21e3a24c27fd3227de25f14
Tanmay <tanmay2020anand@gmail.com>
2023-09-05 21:42:18 +0530

    template add

---
+++ TEMPLATE.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define int long long
+#define YES cout<<"YES \n";
+#define YES cout<<"YES \n";
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL);
+
+typedef vector<ll> vec;
+
+
+// const int MOD = 1e9 + 7;
+// const int INF = LLONG_MAX >> 1;
+
+signed main() 
+
+{
+	
+	int t; 
+	cin >> t;
+	while (t--) 
+	{
+		
+	}
+
+}
+
